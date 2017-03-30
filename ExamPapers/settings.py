279f0b1bdb15909333ca462aa8b8574f0c867262
Dan <vandan193@gmail.com>
2017-03-30 23:20:38 +0800

    a

--- ExamPapers/settings.py
+++ ExamPapers/settings.py
@@ -64,6 +64,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
+PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 
 # URL prefix for static files.
@@ -80,6 +81,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+	os.path.join(PROJECT_ROOT, 'static'),
 )
 
 # List of finder classes that know how to find static files in
@@ -120,7 +122,7 @@ ROOT_URLCONF = 'ExamPapers.urls'
 #WSGI_APPLICATION = 'wsgi.application'
 WSGI_APPLICATION = 'ExamPapers.wsgi.application'
 
-PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
+
 TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
