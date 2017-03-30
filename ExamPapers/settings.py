3f311bb2fd20b25ea27053f4d27404085bc6677b
Dan <vandan193@gmail.com>
2017-03-30 23:39:55 +0800

    a

--- ExamPapers/settings.py
+++ ExamPapers/settings.py
@@ -53,7 +53,8 @@ USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = ''
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
