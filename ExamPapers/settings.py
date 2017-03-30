e470457e5b53edc31e3c48567c7a1248bd3ddf04
Dan <vandan193@gmail.com>
2017-03-30 23:54:07 +0800

    a

--- ExamPapers/settings.py
+++ ExamPapers/settings.py
@@ -53,8 +53,7 @@ USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+MEDIA_ROOT = ''
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
@@ -65,7 +64,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = 'staticfiles'
+STATIC_ROOT = ''
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -118,6 +117,7 @@ TEMPLATE_CONTEXT_PROCESSORS = (
 ROOT_URLCONF = 'ExamPapers.urls'
 
 # Python dotted path to the WSGI application used by Django's runserver.
+#WSGI_APPLICATION = 'wsgi.application'
 WSGI_APPLICATION = 'ExamPapers.wsgi.application'
 
 PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
